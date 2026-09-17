950b1d25821aaf4c035664e488bf495c4c29ff79
Diaaashraf2004 <diaa.ashraf247@gmail.com>
2026-09-17 14:46:35 +0300

    Delete Backup-Now.bat

--- Backup-Now.bat
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-echo Backing up testing environment...
-set TIMESTAMP=%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%
-set TIMESTAMP=%TIMESTAMP: =0%
-set BACKUP_DIR=D:\الشركة\بيئة اختبار_Backup_%TIMESTAMP%
-xcopy "D:\الشركة\بيئة اختبار" "%BACKUP_DIR%" /E /I /H /C /Y
-echo Backup complete! Saved to: %BACKUP_DIR%
-pause
